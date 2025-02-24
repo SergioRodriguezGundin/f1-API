f288533946233dcdbc2ff41085a6e269861abeb3
Sergio-RG <40829363+SergioRodriguezGundin@users.noreply.github.com>
2025-02-24 23:27:58 +0100

    test texts

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -7,19 +7,19 @@ import worker from '../src/index';
 // `Request` to pass to `worker.fetch()`.
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
-describe('Hello World worker', () => {
-	it('responds with Hello World! (unit style)', async () => {
-		const request = new IncomingRequest('http://example.com');
-		// Create an empty context to pass to `worker.fetch()`.
-		const ctx = createExecutionContext();
-		const response = await worker.fetch(request, env, ctx);
-		// Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-		await waitOnExecutionContext(ctx);
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
+describe('F1 API worker', () => {
+  it('responds with this is the f1 api! (unit style)', async () => {
+    const request = new IncomingRequest('http://example.com');
+    // Create an empty context to pass to `worker.fetch()`.
+    const ctx = createExecutionContext();
+    const response = await worker.fetch(request, env, ctx);
+    // Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
+    await waitOnExecutionContext(ctx);
+    expect(await response.text()).toMatchInlineSnapshot(`"this is the f1 api!"`);
+  });
 
-	it('responds with Hello World! (integration style)', async () => {
-		const response = await SELF.fetch('https://example.com');
-		expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
-	});
+  it('responds with this is the f1 api! (integration style)', async () => {
+    const response = await SELF.fetch('https://example.com');
+    expect(await response.text()).toMatchInlineSnapshot(`"this is the f1 api!"`);
+  });
 });
