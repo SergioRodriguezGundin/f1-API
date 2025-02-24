6e84bd73db73402f442cdd77f6efff7fedc3c3fe
Sergio-RG <40829363+SergioRodriguezGundin@users.noreply.github.com>
2025-02-24 23:12:42 +0100

    Moved fetchRequestHandler into a new function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Hono } from 'hono';
+import { Context, Hono } from 'hono';
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 import { appRouter } from './trpc/routers/main';
 import { createContext } from './trpc';
@@ -9,6 +9,15 @@ const createContextWithEnv = async (opts: any, env: any) => {
   return createContext({
     ...opts,
     req: Object.assign(opts.req, { env }),
+  });
+};
+
+const requestHandler = async (c: Context) => {
+  return await fetchRequestHandler({
+    endpoint: '/f1',
+    req: c.req.raw,
+    router: appRouter,
+    createContext: async (opts) => createContextWithEnv(opts, c.env),
   });
 };
 
@@ -23,12 +32,7 @@ app.all('/f1/*', async (c) => {
       return cachedResponse;
     }
 
-    const response = await fetchRequestHandler({
-      endpoint: '/f1',
-      req: request,
-      router: appRouter,
-      createContext: async (opts) => createContextWithEnv(opts, c.env),
-    });
+    const response = await requestHandler(c);
     response.headers.set('Cache-Control', 's-maxage=3600');
 
     c.executionCtx.waitUntil(cache.put(request, response.clone()));
@@ -37,12 +41,7 @@ app.all('/f1/*', async (c) => {
   }
 
   // no-GET (mutations, etc)
-  return await fetchRequestHandler({
-    endpoint: '/f1',
-    req: request,
-    router: appRouter,
-    createContext: async (opts) => createContextWithEnv(opts, c.env),
-  });
+  return await requestHandler(c);
 });
 
 app.get('/', (c) => c.text('F1 API running!'));
